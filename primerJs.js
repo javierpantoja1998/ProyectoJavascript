b7be612148968f2ed0a46775fe08158f99cf5ddd
javierpantoja1998 <cientificodebanco@gmail.com>
2022-10-07 13:22:05 +0200

    first commit

---
+++ primerJs.js
@@ -0,0 +1,20 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <button onclick="mostrarSaludo()"> 
+            Saludar
+        </button>
+    </body>
+    <script>
+            function mostrarSaludo() {
+                
+            }{
+                //COMENTARIOS
+
+                alert("Mensaje de alerta");
+                console.log("Consola de desarrollo preferiblemente");
+            }
+    </script>
+</html>
